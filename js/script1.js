b47c72443db3e395a089905ad904ba5b782e5bab
TNM0001 <tnm7530@gmail.com>
2023-08-15 19:01:33 -0400

    Fixed bug where the video wouldn't go away after playback stops

--- js/script1.js
+++ js/script1.js
@@ -2,10 +2,8 @@ window.onload = function () {
     var video = document.getElementById('intro-video');
     var content = document.getElementById('content');
 
-    // Play the video
     video.play();
 
-    // When the video ends, hide the video and show the content
     video.addEventListener('ended', function () {
         video.style.display = 'none';
         content.style.display = 'block';
