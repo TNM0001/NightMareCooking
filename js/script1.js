4693e8cd14e448f67fa12fc9fbf44cc8f110dc3d
TNM0001 <tnm7530@gmail.com>
2023-08-16 19:51:18 -0400

    Remove animated background to focus on functionality

--- js/script1.js
+++ js/script1.js
@@ -2,18 +2,16 @@ const introVideo = document.getElementById('intro-video');
 const skipButton = document.getElementById('skip-button');
 const content = document.getElementById('content');
 
-introVideo.addEventListener('timeupdate', function() {
-    if (introVideo.currentTime > 3) {
-        skipButton.style.display = 'block';
-    }
+introVideo.addEventListener('play', () => {
+    skipButton.style.display = 'block';
 });
 
 introVideo.addEventListener('ended', () => {
     introVideo.style.display = 'none';
+    skipButton.style.display = 'none';
     content.style.display = 'block';
     setTimeout(() => {
         content.classList.add('fade-in');
-        skipButton.style.display = 'none';
     }, 100);
 });
 
