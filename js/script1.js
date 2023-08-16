86d9fe443b0abd97cfae3f6f531320ee97e4fd1d
TNM0001 <tnm7530@gmail.com>
2023-08-16 19:29:30 -0400

    Updated animation to hopefully fix it

--- js/script1.js
+++ js/script1.js
@@ -2,6 +2,6 @@ const introVideo = document.getElementById('intro-video');
 const content = document.getElementById('content');
 
 introVideo.addEventListener('ended', () => {
+    introVideo.style.display = 'none';
     content.style.display = 'block';
-    content.classList.add('matrix');
 });
