a0e135aae45358d3cfecfae325ecae875005c1f6
TNM0001 <tnm7530@gmail.com>
2023-08-15 20:19:59 -0400

    Added a general overview, updated html and JS

--- js/script1.js
+++ js/script1.js
@@ -1,11 +1,18 @@
 window.onload = function () {
     var video = document.getElementById('intro-video');
     var content = document.getElementById('content');
+    var overview = document.getElementById('overview');
+    var visitorCount = document.getElementById('visitor-count');
+
+    var simulatedVisitorCount = 12345;
+
+    visitorCount.textContent = simulatedVisitorCount;
 
     video.play();
 
     video.addEventListener('ended', function () {
         video.style.display = 'none';
+        overview.style.display = 'none'; // Hide the overview section
         content.style.display = 'block';
     });
 };
